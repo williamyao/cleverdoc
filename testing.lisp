7001bf4e5c0b80099b4ec6cf5f43a847fb31ec02
William Yao <williamyaoh@gmail.com>
2015-09-06 15:03:38 -0500

    implement various messages

--- testing.lisp
+++ testing.lisp
@@ -109,19 +109,6 @@ TEST-LEVEL can be one of:
   (dolist (symbol (get-test-symbols test-level))
     (remhash symbol *tests*)))
 
-(defun test-run-pretty-message (test-run)
-  (if (pass? test-run)
-      (pass-pretty-message test-run)
-      (fail-pretty-message test-run)))
-
-(defgeneric pass-pretty-message (test-run)
-  (:documentation "Message to be printed for a `test-run' that
-meets expectations."))
-
-(defgeneric fail-pretty-message (test-run)
-  (:documentation "Message to be printed for a `test-run' that
-does not meet expectations."))
-
 (defmacro define-test (symbol &body test-body)
   `(%define-test ',symbol
                  (lambda ()
@@ -132,12 +119,19 @@ does not meet expectations."))
         test-function))
 
 
+;; This is more general and is used for equality.
 (record:define test-run ()
   pass?
-  (function-symbol *function*)
-  arguments
   (millisecond-duration (- *milliseconds-run-start-time*
                            (milliseconds-current-time))))
+
+(defgeneric second-duration (test-run)
+  (:method ((test-run test-run))
+    (milliseconds->seconds (millisecond-duration test-run))))
+
+(record:define function-run (test-run)
+  (arguments '())
+  (test-function *function*))
 
 (variable-specification *test-runs*
   "Set of `test-run' objects.
@@ -169,19 +163,141 @@ and run the test body. Good for one `test-run' only."
 
 (defun call-with-run (function) (funcall function))
 
-(defun pass (message)
-  (push (make-instance 'test-run
-                       :pass? t
-                       :message message)
-        *test-runs*))
-
-(defun fail (message)
-  (push (make-instance 'test-run
-                       :pass? nil
-                       :message message)
-        *test-runs*))
+;;; TODO 2015-09-06 williamyaoh@gmail.com
+;;;  - These will have to be general to fit all
+;;;    expectations. Will have to revisit these.
+(defun pass ())
+(defun fail ())
 
 
+;;; Quick note. When testing against multiple value return,
+;;; if the expectant is shorter than the resultant, then it should
+;;; only compare up to the end of the expectant, but if the expectant
+;;; is longer than the resultant, that should be an error.
+
+(defun test-run-duration-message (test-run)
+  (format nil "(~,3F sec)" (second-duration test-run)))
+
+;;; TODO 2015-09-06 williamyaoh@gmail.com
+;;;  - This may be responsible for including the header, which would make
+;;;    sense.
+(defun test-run-pretty-message (test-run)
+  )
+
+(defgeneric pass-header (test-run)
+  (:documentation "Contextual header for a `test-run' that
+meets expectations. Since there's no point printing a detailed message
+for passes, this includes the test duration. These may also be
+disabled entirely by the test-running user.")
+  (:method ((test-run test-run))
+    (format nil
+            "Test met expectations. ~A"
+            (test-run-duration-message test-run))))
+
+(defgeneric fail-pretty-message (test-run)
+  (:documentation "Message to be printed for a `test-run' that
+does not meet expectations.")
+  (:method ((test-run test-run))
+    (format nil
+            "Test did not meet expectations. ~A"
+            (test-run-duration-message test-run))))
+
+(defgeneric fail-header (test-run)
+  (:documentation "Contextual header for a `test-run' that
+does not meet expectations. Does not need to be as specialized as
+the actual failure message.")
+  (:method ((test-run test-run)) "!!!TEST FAILURE!!!"))
+
+(record:define equality-run ()
+  (resultant-values '())
+  (expectant-values '()))
+
+(record:define expect-true-run (test-run equality-run))
+(record:define expect-false-run (test-run equality-run))
+
+(record:define expect-true-function-run (function-run equality-run))
+(record:define expect-false-function-run (function-run equality-run))
+
+(defmethod pass-header ((test-run function-run))
+  (format nil
+          "Test for ~A::~A met expectations. ~A"
+          (package-name (symbol-package (test-function test-run)))
+          (symbol-name (test-function test-run))
+          (test-run-duration-message test-run)))
+
+(defmethod fail-header ((test-run function-run))
+  (format nil
+          "~A Test for ~A::~A did not meet expectations:"
+          (call-next-method)
+          (package-name (symbol-package (test-function test-run)))
+          (symbol-name (test-function test-run))))
+
+(defun agree-length (resultant-values expected-values)
+  "Make RESULTANT 'agree' with EXPECTED in length.
+Returns a copy of RESULTANT, but the same length as EXPECTED or
+shorter."
+  (map 'list
+       (lambda (r e) (declare (ignore e)) r)
+       resultant-values
+       expected-values))
+
+(defun maybe-multiple-value-message (list-of-values)
+  "Return a simpler message for single values, and a more
+accurate but complicated one for multiple values."
+  (format nil
+          "~:[multiple values (~;~]~{~S~^, ~}~:[)~;~]"
+          (single-p list-of-values)
+          list-of-values
+          (single-p list-of-values)))
+
+(defmethod fail-pretty-message ((test-run expect-true-run))
+  (let ((trimmed-resultant-values (agree-length (resultant-values test-run)
+                                                (expectant-values test-run))))
+    (format nil
+            "Value(s) did not equal; left side was ~A but right side was ~A ~A"
+            (maybe-multiple-value-message trimmed-resultant-values)
+            (maybe-multiple-value-message (expectant-values test-run))
+            (test-run-duration-message test-run))))
+
+;;; TODO 2015-09-06 williamyaoh@gmail.com
+;;;  - These messages look pretty similar for failure and passing.
+;;;    Might want to make them more distinctive somehow. Maybe using
+;;;    xterm colors?
+(defmethod fail-pretty-message ((test-run expect-false-run))
+  (let ((trimmed-resultant-values (agree-length (resultant-values test-run)
+                                                (expectant-values test-run))))
+    (format nil
+            "Value(s) equalled, but were not supposed to; ~
+             left side was ~A and right side was ~A ~A"
+            (maybe-multiple-value-message trimmed-resultant-values)
+            (maybe-multiple-value-message (expectant-values test-run))
+            (test-run-duration-message test-run))))
+
+(defmethod fail-pretty-message ((test-run expect-true-function-run))
+  (let ((trimmed-resultant-values (agree-length (resultant-values test-run)
+                                                (expectant-values test-run))))
+    (format nil
+            "(~A~{ ~S~}) returned ~A, but expected ~A ~A"
+            (test-function test-run)
+            (arguments test-run)
+            (maybe-multiple-value-message trimmed-resultant-values)
+            (maybe-multiple-value-message (expectant-values test-run))
+            (test-run-duration-message test-run))))
+
+(defmethod fail-pretty-message ((test-run expect-false-function-run))
+  (let ((trimmed-resultant-values (agree-length (resultant-values test-run)
+                                                (expectant-values test-run))))
+    (format nil
+            "(~A~{ ~S~}) returned ~A, but was not supposed to ~A"
+            (test-function test-run)
+            (arguments test-run)
+            (maybe-multiple-value-message trimmed-resultant-values)
+            (test-run-duration-message test-run))))
+
+
+(defun milliseconds->seconds (milliseconds)
+  (/ milliseconds 1000))
+
 (defun milliseconds-current-time ()
   (/ (get-internal-real-time)
      (/ internal-time-units-per-second
@@ -191,3 +307,7 @@ and run the test body. Good for one `test-run' only."
   "X is N percent of Y.
 Return N, as an integer."
   (round (* 100 (/ x y))))
+
+(defun single-p (list)
+  "Check if LIST is a list with only one element."
+  (null (rest list)))
